79659f0ad8c21b68821d79c0fa60d50610064a57
Oneq <oneq@Sihans-Laptop.local>
2026-08-17 02:11:09 +0600

    Stage 4: full CRUD

--- main.py
+++ main.py
@@ -33,17 +33,54 @@ async def get_tasks():
 @app.get("/tasks/{id}")
 async def get_task(id: int):
     try:
-        return {"task": tasks[id-1]}
+        return {"task": [task for task in tasks if task["id"] == id][0]}
     except IndexError:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Task {id} not found")
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, 
+            detail=f"Task {id} not found"
+        )
 
 class taskModel(BaseModel):
     title: str
+    done: bool = False
 
 @app.post("/tasks", status_code=status.HTTP_201_CREATED)
 async def create_task(task: taskModel):
     if task.title == "":
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Task title cannot be empty")
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, 
+            detail="Task title cannot be empty"
+        )
     new_task = {"id": len(tasks) + 1, "title": task.title, "done": False}
     tasks.append(new_task)
     return {"message" : "Created"}
+
+@app.put("/tasks/{id}")
+async def update_task(id: int, task: taskModel):
+    for item in tasks:
+        if item["id"] == id:
+            if task.title == "":
+                raise HTTPException(
+                    status_code=status.HTTP_400_BAD_REQUEST, 
+                    detail="Invalid body"
+                )
+            if item["title"] != "":
+                item["title"] = task.title
+            if item["done"] != "":
+                item["done"] = task.done
+            return {"message": "done", "tasks": tasks}
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND, 
+        detail=f"Task {id} not found"
+    )
+
+@app.delete("/tasks/{id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_task(id: int):
+    for item in tasks:
+        if item["id"] == id:
+            tasks.remove(item)
+            return {"message": "No Content"}
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND, 
+        detail=f"Task {id} not found"
+    )
